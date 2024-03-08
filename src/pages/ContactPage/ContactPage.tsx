688004751b81e1573552cee1657c43a1faf6a221
ciisiq <cii_3@hotmail.com>
2024-03-08 12:54:44 +0000

    Fix rootElement, Style: make some style to contactPage

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,42 +1,41 @@
+import React, { useRef, useEffect } from 'react';
+import { PopupButton } from 'react-calendly';
 import './ContactPage.css';
-import { InlineWidget, PopupWidget, PopupButton } from 'react-calendly';
 
-export default function ContactPage() {
+const ContactPage: React.FC = () => {
+  const rootElementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const rootElement = document.getElementById('root');
+    if (rootElement) {
+      rootElementRef.current = rootElement as HTMLDivElement;
+    }
+  }, []);
+
   return (
     <section className="contactpage">
-      <h2>Thank you!</h2>
-      <p>Contact me by this form or use my social medial.</p>
-      <p>Twitter Logo</p>
-      <p>Linkedin LOGO</p>
-      <p>Behance Logo</p>
+      <div className="left">
+        <h2>Thank you!</h2>
+        <p>Contact me by this Schedule button or use my social medial.</p>
+        <div className="cont-logos">
+          <p>Twitter Logo</p>
+          <p>Linkedin LOGO</p>
+          <p>Behance Logo</p>
+        </div>
+      </div>
 
-      {/* <!-- Calendly inline widget begin --> */}
-      {/* <div className="inline-widget">
-        <InlineWidget url="https://calendly.com/cii_3/15min?background_color=1f2135&text_color=00b0c5&primary_color=00b0c5" />
-      </div> */}
+      <div className="verticalLine"></div>
 
-      {/* <div className="popup-widget">
-        <PopupWidget
-          url="https://calendly.com/cii_3/15min?background_color=1f2135&text_color=00b0c5&primary_color=00b0c5"
-          rootElement={document.getElementById('root')}
-          text="Schedule Time with me"
-          textColor="#ffffff"
-          color="#00b0c5"
-        />
-      </div> */}
-
-      <div className="App">
+      <div className="calendar">
+        <h2>Contact me 👇</h2>
         <PopupButton
           url="https://calendly.com/cii_3/15min?background_color=1f2135&text_color=00b0c5&primary_color=00b0c5"
-          /*
-           * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-           * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-           */
-          rootElement={document.getElementById('root')}
+          rootElement={rootElementRef.current as HTMLElement}
           text="Click here to schedule!"
         />
       </div>
-      {/* <!-- Calendly inline widget end --> */}
     </section>
   );
-}
+};
+
+export default ContactPage;
